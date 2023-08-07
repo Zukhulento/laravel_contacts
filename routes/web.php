75881615c484ab55b6212f50b3e9a0fc4642cec3
Zukhulento <luis.matus.jr@gmail.com>
2023-08-07 10:15:08 -0600

    Store contact

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Models\Contact;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,11 +21,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/ruta', function () {
-    return view('welcome');
-});
-
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/contacts/create',[ContactController::class,'create'])->name('contacts.create');
+Route::get('/contacts',[ContactController::class,'store'])->name('contacts.store');
